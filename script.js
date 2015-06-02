c34c0acb77e6a35e01a043ceeffe7672c86add99
bertob <bertob@zenbook>
2015-06-02 16:10:54 +0200

    [minor] remove useless code, formatting

--- script.js
+++ script.js
@@ -1,44 +1,33 @@
-console.log('lollll.');
-
-//var navigator.getUserMedia = navigator.mozGetUserMedia;
-
 var record = document.getElementById('start');
 var stop = document.getElementById('stop');
 var video = document.getElementById('player');
-video.setAttribute('controls', '');
+//video.setAttribute('controls', '');
 
-//if (navigator.getUserMedia) {
-if (true) {
-  //console.log('getUserMedia supported.');
+var constraints = {video: true};
 
-  var constraints = { video: true };
+var onSuccess = function(stream) {
+  console.log("stream", stream);
+  var mediaRecorder = new MediaRecorder(stream);
 
-  var onSuccess = function(stream) {
-    console.log("stream", stream);
-    var mediaRecorder = new MediaRecorder(stream);
-
-    record.onclick = function() {
-      mediaRecorder.start();
-      console.log("recorder started");
-    }
-
-    stop.onclick = function() {
-      mediaRecorder.stop();
-      console.log("recorder stopped");
-    }
-
-    mediaRecorder.ondataavailable = function(e) {
-      console.log("data available after MediaRecorder.stop() called.");
-
-      video.src = window.URL.createObjectURL(e.data);
-    }
-  };
-
-  var onError = function(err) {
-    console.log('The following error occured: ' + err);
+  record.onclick = function() {
+    mediaRecorder.start();
+    console.log("recorder started");
   }
 
-  navigator.mozGetUserMedia(constraints, onSuccess, onError);
-} else {
-   console.log('getUserMedia not supported on your browser!');
+  stop.onclick = function() {
+    mediaRecorder.stop();
+    console.log("recorder stopped");
+  }
+
+  mediaRecorder.ondataavailable = function(e) {
+    console.log("data available after MediaRecorder.stop() called.");
+
+    video.src = window.URL.createObjectURL(e.data);
+  }
+};
+
+var onError = function(err) {
+  console.log('The following error occured: ' + err);
 }
+
+navigator.mozGetUserMedia(constraints, onSuccess, onError);
